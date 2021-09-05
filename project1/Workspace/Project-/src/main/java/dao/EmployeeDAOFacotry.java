22643fe49abec4ff978e12e65273f342e85c5422
jdlovin <josh.lovin@revature.net>
2021-09-05 13:21:31 -0400

    adding ticket impl

---
+++ project1/Workspace/Project-/src/main/java/dao/EmployeeDAOFacotry.java
@@ -0,0 +1,16 @@
+package dao;
+
+public class EmployeeDAOFactory {
+
+    private static EmployeeDAO dao;
+
+    private EmployeeDAOFactory(){
+
+    }
+
+    public static EmployeeDAO getEmployeeDAO(){
+        if(dao == null)
+            dao = new EmployeeDAOImpl();
+        return dao;
+    }
+}
--- project1/Workspace/Project-/src/main/java/dao/TicketDAO.java
+++ project1/Workspace/Project-/src/main/java/dao/TicketDAO.java
@@ -5,6 +5,8 @@ import java.util.List;
 import models.Ticket;
 
 public interface TicketDAO {
+	void addTicket(Ticket ticket);
+	void updateTicket(Ticket ticekt);
 	List<Ticket> getTickets();
 	List<Ticket> getEmployeesTickets(String email,String password);
 	}
