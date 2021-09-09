5e0ea02602040f3f7200088d6630d0514e53a300
Mpartx21 <miguel.p@revature.net>
2021-09-08 19:05:47 -0700

    Merge branch 'final-branch' of https://github.com/Mpartx21/Project-1 into final-branch

--- project1/Workspace/Project-/src/main/java/servlets/EmployeeHomeServlet.java
+++ project1/Workspace/Project-/src/main/java/servlets/EmployeeHomeServlet.java
@@ -4,16 +4,25 @@ import java.io.IOException;
 import java.util.List;
 
 import dao.TicketDAOFactory;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import models.Employee;
 import models.Ticket;
-@WebServlet(name = "/employeehome")
+
 public class EmployeeHomeServlet extends HttpServlet{
 	
-	protected void doPost(HttpServletRequest request,HttpServletResponse response) throws IOException {
-		List<Ticket> tickets = TicketDAOFactory.getTicketDAO().getEmployeesTicketsByID(0)
+	protected void doPost(HttpServletRequest request,HttpServletResponse response) throws IOException, ServletException {
+		
+		Employee emp = (Employee) request.getSession().getAttribute("employee");
+		
+		List<Ticket> tickets = TicketDAOFactory.getTicketDAO().getEmployeesTicketsByID(emp.getId());
+		
+		request.getSession().setAttribute("tickets", tickets);
+		
+		request.getRequestDispatcher("EmployeeHome.jsp").forward(request, response);
 				
 				
 	}
